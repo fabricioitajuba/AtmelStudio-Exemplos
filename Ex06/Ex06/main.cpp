3f0d3bc185567b0b2f9e15091243ea8ef041d7a4
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2022-09-21 11:11:31 -0300

    Add files via upload

---
+++ Ex06/Ex06/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Ex06.cpp
+ *
+ * Created: 21/09/2022 09:09:16
+ * Author : Desenvolvimento
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define SetBit(RES, BIT)(RES |= (1 << BIT))   // Por BIT em nível alto
+#define ClrBit(RES, BIT)(RES &= ~ (1 << BIT)) // Por BIT em nível baixo
+#define TstBit(RES, BIT)(RES &  (1 << BIT)) // testar BIT, retorna 0 ou 1
+#define CplBit(RES, BIT)(RES ^= (1 << BIT)) // Inverter estado do BIT
+
+int main(void){
+	
+	SetBit(DDRB, PB5);  //Pino 13 como saída.
+	while( true ){
+		ClrBit(PORTB, PB5); // bit PB5 LOM
+		_delay_ms(100);
+		SetBit(PORTB, PB5); // bit PB5 HIGH
+		_delay_ms(100);
+	}
+}
