6631a565988ab982988b16f46dd13922db4463c1
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2022-10-17 16:58:56 -0300

    Exemplo que plota valor no display LCD

---
+++ Ex03-LCD5-com-recursos-teste/Ex03-LCD5-com-recursos-teste/main.c
@@ -0,0 +1,44 @@
+/*
+ * Ex03 - LCD5 - com recursos - teste.c
+ *
+ * Created: 17/10/2022 14:54:23
+ * Author : Fabrício Ribeiro
+ */ 
+
+#define F_CPU 16000000ul
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+#include <math.h>
+#include "Lcdlbr/lcd.h"
+
+int   outlcd(char c, FILE *stream);
+FILE mystdout = FDEV_SETUP_STREAM(outlcd, NULL, _FDEV_SETUP_WRITE);
+
+void lcdini(void)
+{
+	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+	stdout = &mystdout;
+}
+
+int outlcd(char c, FILE *stream){
+	lcd_putc(c);
+	return 0;
+}
+
+int main(void)
+{
+	float n=2.56;
+	lcdini();
+	printf("Hello World!!");
+	printf("\nn=%2.2f", n);
+
+	while(1)
+	{
+		//TODO:: Just sit here forever
+	}
+}
+
