77c47b5e3fe8d870a22f95f2ac13978f1846f95c
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2022-10-18 16:01:01 -0300

    LCD melhor exemplo de todos

---
+++ Ex03-LCD6/Ex03-LCD6/main.c
@@ -0,0 +1,41 @@
+/*
+ * Ex03 - LCD6
+ *
+ * Created: 17/10/2022 14:54:23
+ * Author : Fabrício Ribeiro
+ */ 
+
+#define  F_CPU 2000000UL
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "Lcdlbr/lcd.h"
+
+int   outlcd(char c, FILE *stream);
+FILE mystdout = FDEV_SETUP_STREAM(outlcd, NULL, _FDEV_SETUP_WRITE);
+
+int outlcd(char c, FILE *stream){
+	lcd_putc(c);
+	return 0;
+}
+
+int main(void)
+{
+	float n=3.1415;
+
+	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+	stdout = &mystdout;
+		
+	while(1)
+	{
+		printf("Hello World!!");
+		printf("\nn=%2.2f", n);
+		_delay_ms(1000);
+		lcd_clrscr();
+		
+		printf("teste");
+		printf("\ndisplay");
+		_delay_ms(1000);
+		lcd_clrscr();
+	}
+}
