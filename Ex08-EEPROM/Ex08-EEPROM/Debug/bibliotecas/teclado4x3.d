b9a276f0432882f9870478b2cb64538569ba6498
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2022-10-21 14:47:15 -0300

    LCD + TECLADO + EEPROM

---
+++ Ex08-EEPROM/Ex08-EEPROM/Debug/bibliotecas/teclado4x3.d
@@ -0,0 +1,23 @@
+bibliotecas/teclado4x3.d bibliotecas/teclado4x3.o: \
+ ../bibliotecas/teclado4x3.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../bibliotecas/teclado4x3.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../bibliotecas/teclado4x3.h:
--- Ex08-EEPROM/Ex08-EEPROM/main.c
+++ Ex08-EEPROM/Ex08-EEPROM/main.c
@@ -15,7 +15,26 @@
  #include "bibliotecas/gpio.h"
  #include "bibliotecas/teclado4x3.h"
 
- int   outlcd(char c, FILE *stream);
+ /*
+ Inicialização da EEPROM
+ O Atmel studio gera 3 arquivos após a compilação: .hex, .elf, .eep
+ 
+ .hex -> programa compilado
+ .epp -> eeprom
+ .elf = .hex + .epp + fuses
+ 
+ No minipro, carrega-se o .hex em "Code Memo" e o .epp em "Data Memo". Ambos no formato: "INTEL HEX"
+ No proteus, pode-se carregar o .hex ou o .elf, o .elf irá inicializar a eeprom com os valores inicializados.
+ */
+ 
+ //Formas de inicializar a eeprom
+ __attribute__((section(".eeprom"))) uint8_t eepContent[8] = {0x46, 0x41, 0x42, 0x52, 0x49, 0x43, 0x49, 0x4F}; //Inicializa as 8 primeiras posições
+ uint8_t EEMEM Var1 = 7;	//Inicializa a próxima localização 8bits
+ uint8_t EEMEM Var2 = 3;	//Inicializa a próxima localização 8bits
+ uint8_t EEMEM Var3 = 76;	//Inicializa a próxima localização 8bits
+ int eevar EEMEM = 1976;	//Inicializa a próxima localização 16bits
+
+ int outlcd(char c, FILE *stream);
  FILE mystdout = FDEV_SETUP_STREAM(outlcd, NULL, _FDEV_SETUP_WRITE);
 
  int outlcd(char c, FILE *stream){
@@ -36,7 +55,7 @@
     printf("End:Dado");
 	
 	offset=0;
-	dado = eeprom_read_byte(end+offset);
+	dado = eeprom_read_byte((uint8_t*)(end+offset));
 	printf("\n%02Xh=%02Xh", end+offset, dado);
 	
     while (1)
@@ -45,14 +64,14 @@
 	    
 	    if(tecla == 1){		
 			offset++;
-			dado = eeprom_read_byte(end+offset);
+			dado = eeprom_read_byte((uint8_t*)(end+offset));
 			lcd_gotoxy(0, 1);
 			printf("%02Xh=%02Xh", end+offset, dado);
 			lcd_gotoxy(4, 1);
 	    }
 	    else if(tecla == 4){
 			offset--;
-			dado = eeprom_read_byte(end+offset);
+			dado = eeprom_read_byte((uint8_t*)(end+offset));
 			lcd_gotoxy(0, 1);
 			printf("%02Xh=%02Xh", end+offset, dado);
 			lcd_gotoxy(4, 1);
@@ -75,7 +94,7 @@
 				}
 				
 			}while(n_digito != 2);
-			eeprom_write_byte (end+offset, aux);
+			eeprom_write_byte ((end+offset), aux);
 			lcd_command(LCD_DISP_ON);
 	    }		
     }
